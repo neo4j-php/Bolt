ceb8a013c8b6c35dfbff12ef3367a1b6fd9b42cb
Michal Stefanak <stefanak.michal@gmail.com>
2020-12-27 10:41:19 +0100

    Added 4.2 version support

---
+++ src/protocol/V4_2.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bolt\protocol;
+
+/**
+ * Class Protocol version 4.2
+ *
+ * @author Michal Stefanak
+ * @link https://github.com/stefanak-michal/Bolt
+ * @package Bolt\protocol
+ */
+class V4_2 extends V4_1
+{
+
+}
