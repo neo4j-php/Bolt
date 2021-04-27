253b3b6beafe54dcda8c7b12594cbf08474faf24
Michal Stefanak <stefanak.michal@gmail.com>
2021-04-27 16:37:35 +0200

    added interface annotations

--- src/structures/IStructure.php
+++ src/structures/IStructure.php
@@ -1,9 +1,14 @@
 <?php
 
-
 namespace Bolt\structures;
 
-
+/**
+ * interface IStructure
+ *
+ * @author Michal Stefanak
+ * @link https://github.com/neo4j-php/Bolt
+ * @package Bolt\structures
+ */
 interface IStructure
 {
     public function __toString () : string;
