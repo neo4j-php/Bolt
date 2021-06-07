ebd86fba0cbfef7ffa8f13a79b96504624ff908c
Michal Stefanak <stefanak.michal@gmail.com>
2021-06-07 16:29:51 +0200

    added support for v4.3

---
+++ src/protocol/V4_3.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bolt\protocol;
+
+/**
+ * Class Protocol version 4.3
+ *
+ * @author Michal Stefanak
+ * @link https://github.com/neo4j-php/Bolt
+ * @package Bolt\protocol
+ */
+class V4_3 extends V4_1
+{
+
+}
