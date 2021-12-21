c0d087939a0c6e3c7054ae8ca9627059fd4b2901
Michal Stefanak <stefanak.michal@gmail.com>
2021-12-21 13:12:51 +0100

    updated url

--- src/protocol/V2.php
+++ src/protocol/V2.php
@@ -6,7 +6,7 @@ namespace Bolt\protocol;
  * Class Protocol version 2
  *
  * @author Michal Stefanak
- * @link https://github.com/stefanak-michal/Bolt
+ * @link https://github.com/neo4j-php/Bolt
  * @see https://7687.org/bolt/bolt-protocol-message-specification-2.html
  * @package Bolt\protocol
  */
