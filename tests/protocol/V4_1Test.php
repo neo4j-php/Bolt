f7d97205ba2be7d1779d6dfb3b9837eec7ae898f
Michal Stefanak <stefanak.michal@gmail.com>
2021-12-22 11:01:38 +0100

    Updated test to follow Factory design pattern. Changed behaviour of some tests. Added more tests.

--- tests/protocol/V4_1Test.php
+++ tests/protocol/V4_1Test.php
@@ -3,6 +3,7 @@
 namespace Bolt\tests\protocol;
 
 use Bolt\protocol\V4_1;
+use Exception;
 
 /**
  * Class V4_1Test
@@ -22,9 +23,8 @@ class V4_1Test extends \Bolt\tests\ATest
     /**
      * @return V4_1
      */
-    public function test__construct()
+    public function test__construct(): V4_1
     {
-        \Bolt\helpers\Auth::$userAgent = 'Test/1.0';
         $cls = new V4_1(new \Bolt\PackStream\v1\Packer, new \Bolt\PackStream\v1\Unpacker, $this->mockConnection());
         $this->assertInstanceOf(V4_1::class, $cls);
         return $cls;
@@ -33,14 +33,17 @@ class V4_1Test extends \Bolt\tests\ATest
     /**
      * @depends test__construct
      * @param V4_1 $cls
-     * @throws \Exception
      */
     public function testHello(V4_1 $cls)
     {
         self::$readArray = [1, 2, 0];
-        self::$writeBuffer = [hex2bin('0051b101a58a757365725f6167656e7488546573742f312e3086736368656d65856261736963897072696e636970616c84757365728b63726564656e7469616c738870617373776f726487726f7574696e67a00000')];
+        self::$writeBuffer = [hex2bin('0048b101a48a757365725f6167656e7488626f6c742d70687086736368656d65856261736963897072696e636970616c84757365728b63726564656e7469616c738870617373776f7264')];
 
-        $this->assertIsArray($cls->hello(\Bolt\helpers\Auth::basic('user', 'password'), []));
+        try {
+            $this->assertIsArray($cls->hello(\Bolt\helpers\Auth::basic('user', 'password'), []));
+        } catch (Exception $e) {
+            $this->markTestIncomplete($e->getMessage());
+        }
     }
 
     /**
@@ -51,11 +54,11 @@ class V4_1Test extends \Bolt\tests\ATest
     {
         self::$readArray = [4, 5, 0];
         self::$writeBuffer = [
-            hex2bin('0051b101a58a757365725f6167656e7488546573742f312e3086736368656d65856261736963897072696e636970616c84757365728b63726564656e7469616c738870617373776f726487726f7574696e67a00000'),
-            hex2bin('0002b00e0000')
+            hex2bin('0048b101a48a757365725f6167656e7488626f6c742d70687086736368656d65856261736963897072696e636970616c84757365728b63726564656e7469616c738870617373776f7264'),
+            hex2bin('0002b00e')
         ];
 
-        $this->expectException(\Exception::class);
+        $this->expectException(Exception::class);
         $this->expectExceptionMessage('some error message (Neo.ClientError.Statement.SyntaxError)');
         $cls->hello(\Bolt\helpers\Auth::basic('user', 'password'), []);
     }
--- tests/protocol/V4_4Test.php
+++ tests/protocol/V4_4Test.php
@@ -3,6 +3,7 @@
 namespace Bolt\tests\protocol;
 
 use Bolt\protocol\V4_4;
+use Exception;
 
 /**
  * Class V4_4Test
@@ -22,11 +23,27 @@ class V4_4Test extends \Bolt\tests\ATest
     /**
      * @return V4_4
      */
-    public function test__construct()
+    public function test__construct(): V4_4
     {
         $cls = new V4_4(new \Bolt\PackStream\v1\Packer, new \Bolt\PackStream\v1\Unpacker, $this->mockConnection());
         $this->assertInstanceOf(V4_4::class, $cls);
         return $cls;
+    }
+
+    /**
+     * @depends test__construct
+     * @param V4_4 $cls
+     */
+    public function testRoute(V4_4 $cls)
+    {
+        self::$readArray = [1, 2, 0];
+        self::$writeBuffer = [hex2bin('0020b366a187616464726573738e6c6f63616c686f73743a3736383790a1826462c0')];
+
+        try {
+            $this->assertIsArray($cls->route(['address' => 'localhost:7687'], [], ['db' => null]));
+        } catch (Exception $e) {
+            $this->markTestIncomplete($e->getMessage());
+        }
     }
 
 }
