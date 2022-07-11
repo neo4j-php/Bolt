75c5a01d19a53aef17216599e8627ee30e22d415
Michal Stefanak <stefanak.michal@gmail.com>
2022-07-11 10:54:55 +0200

    Changed Packer from return style into yield. Support for iterator query parameter.

---
+++ src/PackStream/PackListGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\PackStream;
+
+/**
+ * Class PackListGenerator
+ * @author Michal Stefanak
+ * @link https://github.com/neo4j-php/Bolt
+ * @package Bolt\PackStream
+ */
+interface PackListGenerator extends \Iterator, \Countable
+{
+
+}
