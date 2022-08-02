5c27e98ce3397962d47798f98a96a3cb1f74567c
Michal Stefanak <stefanak.michal@gmail.com>
2022-08-02 12:08:09 +0200

    Updated test to see new changes in action.

--- tests/PerformanceTest.php
+++ tests/PerformanceTest.php
@@ -21,21 +21,32 @@ class PerformanceTest extends TestCase
         $amount = 50000;
 
         try {
-            $conn = new StreamSocket($GLOBALS['NEO_HOST'] ?? 'localhost', $GLOBALS['NEO_PORT'] ?? 7687);
+            $conn = new StreamSocket($GLOBALS['NEO_HOST'] ?? 'localhost', $GLOBALS['NEO_PORT'] ?? 7687, 60);
+            /** @var \Bolt\protocol\V4_3|\Bolt\protocol\V4_4 $protocol */
             $protocol = (new Bolt($conn))->build();
-            $this->assertNotEmpty($protocol->hello(Auth::basic($GLOBALS['NEO_USER'], $GLOBALS['NEO_PASS'])));
+            $this->assertEquals(\Bolt\protocol\Response::SIGNATURE_SUCCESS, $protocol->hello(Auth::basic($GLOBALS['NEO_USER'], $GLOBALS['NEO_PASS']))->getSignature());
 
             $generator = new RandomDataGenerator($amount);
-            $protocol->run('UNWIND $x as x RETURN x', ['x' => $generator]);
+            $protocol->run('UNWIND $x as x RETURN x', ['x' => $generator])->getResponse();
 
             $count = 0;
-            while ($count < $amount) {
-                ++$count;
-                $protocol->pull(['n' => 1]);
+            while (true) {
+                $response = $protocol->pull(['n' => 1])->getResponse();
+                $this->assertEquals(\Bolt\protocol\Response::SIGNATURE_RECORD, $response->getSignature());
+                $meta = $protocol->getResponse();
+                $this->assertEquals(\Bolt\protocol\Response::SIGNATURE_SUCCESS, $meta->getSignature());
+
+                $count++;
+
+                if ($meta->getContent()['has_more'] ?? false)
+                    continue;
+                else
+                    break;
             }
 
             $this->assertEquals($amount, $count);
         } catch (\Exception $e) {
+            var_dump($e->getTraceAsString());
             $this->markTestIncomplete($e->getMessage());
         }
     }
