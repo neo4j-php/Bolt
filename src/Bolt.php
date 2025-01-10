204009446ac2f85c70091a48998572d7a03eada4
Michal Stefanak <stefanak.michal@gmail.com>
2025-01-10 10:57:09 +0100

    removed unnecessary check

--- src/Bolt.php
+++ src/Bolt.php
@@ -198,7 +198,7 @@ final class Bolt
     /**
      * Read and compose selected protocol version
      */
-    private function unpackProtocolVersion(string $bytes): ?string
+    private function unpackProtocolVersion(string $bytes): string
     {
         $result = [];
 
@@ -210,8 +210,7 @@ final class Bolt
             array_shift($result);
         }
 
-        $version = implode('.', array_reverse($result));
-        return in_array($version, $this->protocolVersions) ? $version : null;
+        return implode('.', array_reverse($result));
     }
 
     /**
